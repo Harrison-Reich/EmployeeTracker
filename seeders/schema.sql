3174f509fe319aad52d122cddcd3f3548e1f5574
Harrison Reich <hreich1998@gmail.com>
2022-01-09 17:07:35 -0800

    completed role table

--- seeders/schema.sql
+++ seeders/schema.sql
@@ -9,6 +9,10 @@ CREATE TABLE department (
 
 CREATE TABLE role (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
