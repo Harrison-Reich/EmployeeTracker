5702af4af14f2a4e70248e12c540a02328b852fc
Harrison Reich <hreich1998@gmail.com>
2022-01-10 09:34:03 -0800

    code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2')
 const db = mysql.createConnection('mysql://root:rootroot@localhost:3306/employee_db')
 
 const start = () => {
-  inquirer.prompt ([
+  inquirer.prompt([
     {
       type: 'list',
       message: 'What would you like to do?',
@@ -13,42 +13,50 @@ const start = () => {
       name: 'todo'
     }
   ])
-  .then(({ todo }) => {
-    switch (todo) {
+    .then(({ todo }) => {
+      switch (todo) {
 
-      case 'View An Employee':
-        viewEmployees()
-        break;
+        case 'View An Employee':
+          viewEmployees()
+          break;
 
-      case 'View A Department':
-        viewDepartments()
-        break;
+        case 'View A Department':
+          viewDepartments()
+          break;
 
-      case 'Add Employee':
-        addEmployees()
-        break;
+        case 'Add Employee':
+          addEmployees()
+          break;
 
-      case 'Update Employee Role':
-        updateEmployeeRoles()
-        break;
+        case 'Update Employee Role':
+          updateEmployeeRoles()
+          break;
 
-      case 'View All Roles':
-        seeRoles()
-        break;
+        case 'View All Roles':
+          seeRoles()
+          break;
 
-      case 'Add A Role':
-        addRole()
-        break;
+        case 'Add A Role':
+          addRole()
+          break;
 
-      case 'Add A Department':
-        addDepartment()
-        break;
+        case 'Add A Department':
+          addDepartment()
+          break;
 
-      case 'Exit':
-        end()
-        break;
-    }
-  })
+        case 'Exit':
+          end()
+          break;
+      }
+    })
 }
 
 start()
+
+function seeDepartments() {
+  db.query('SELECT * FROM department', (err, departments) => {
+    if (err) { console.log(err) }
+    console.table(departments)
+    start()
+  })
+}
