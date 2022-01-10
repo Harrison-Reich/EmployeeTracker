1d80d80c419d36af68680256e6c3424c335a5565
Harrison Reich <hreich1998@gmail.com>
2022-01-09 17:15:58 -0800

    code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const inquirer = require('inquirer');
 const { prompt } = require('inquirer');
 const cTable = require('console.table');
+const mysql = require('mysql2')
+const db = mysql.createConnection('')
