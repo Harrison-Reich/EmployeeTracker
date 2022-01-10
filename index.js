bbb84521bff7f9de1ba84de39f5b05f7210150e8
Harrison Reich <hreich1998@gmail.com>
2022-01-10 09:23:49 -0800

    code

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const { prompt } = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql2')
 const db = mysql.createConnection('mysql://root:rootroot@localhost:3306/employee_db')
+
+const start = () => {
+  inquirer.prompt ([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ['View All Employee', 'View All Department', 'View All Roles', 'Add Employee', 'Add a Role', 'Add a Department', 'Update Employee Role', 'Exit'],
+      name: 'todo'
+    }
+  ])
+  .
+}
